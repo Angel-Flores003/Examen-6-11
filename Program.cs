8f61ebfa4847628cf0179e9a5c27f4962c6fce15
Angel-Flores003 <angel-abraham.flores.7e8@itb.cat>
2025-11-06 12:06:06 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        const string MsgCoinFound = "You found a coin";
+        const string MsgFoundTrap = "You found a trap";
+        const string MsgWin = "You've won!";
+        const string MsgLost = "You've lost!";        
+        const int MaxCoins = 5;        
+        int lives = 3, coins = 0, prob;
+        Random rand = new Random();
+        bool trap = false;
+
+        do
+        {
+            prob = rand.Next(1, 11);
+            if (prob <= 5)
+            {
+                Console.WriteLine(MsgFoundTrap);
+                lives--;
+                Console.WriteLine($"Now you have {lives} lives");
+                Thread.Sleep(1000);
+            }
+            else
+            {
+                prob = rand.Next(1, 11);
+                if (prob <= 7)
+                {
+                    Console.WriteLine(MsgCoinFound);
+                    coins++;
+                    Console.WriteLine($"Now you have {coins} coins");
+                    Thread.Sleep(1000);
+                }
+            }
+        } while (lives > 0 && coins != MaxCoins);
+        if (coins == MaxCoins)
+        {
+            Console.WriteLine(MsgWin);
+        }
+        else
+        {
+            Console.WriteLine(MsgLost);
+        }
+    }
+}
